8918ac01a13e95259868acfe128395de23336624
Aswin Kalarickal <mail@aswink.in>
2017-08-29 12:18:14 +0530

    Fixed issue with hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const SRC_DIR = path.resolve(__dirname, 'src');
 const config = {
     entry: [
         'webpack/hot/dev-server',
-        'webpack-hot-middleware/client',
+        'webpack-hot-middleware/client?reload=true',
         SRC_DIR + '/app/index.js'
     ],
     output: {
